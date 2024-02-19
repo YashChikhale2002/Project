307799efe56c68d564c35b560afc5dcb8ddee15f
yashchikhale2002 <yashchikhale711@gmail.com>
2024-02-19 18:12:17 +0530

    changes in globals.css and replace the sql file as well. pull from supabase to shema.ts

--- bitlearnersdoc/src/lib/supabase/schema.ts
+++ bitlearnersdoc/src/lib/supabase/schema.ts
@@ -1,4 +1,7 @@
-import {pgTable, text, timestamp, uuid} from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
+import {boolean, integer, jsonb, pgTable, text, timestamp, uuid} from 'drizzle-orm/pg-core';
+import { prices, subscriptionStatus, users } from '../../../migrations/schema';
+
 
 export const workspaces = pgTable('workspaces', {
     id: uuid('id').defaultRandom().primaryKey().notNull(),
@@ -8,7 +11,7 @@ export const workspaces = pgTable('workspaces', {
     }),
     workspaceOwner:uuid('workspace_owner').notNull(),
     title: text('title').notNull(),
-    inconId:text ('icon_id').notNull(),
+    iconid:text ('icon_id').notNull(),
     data: text('data'),
     inTrash:text('in_trash'),
     logo:text('logo'),
@@ -22,7 +25,7 @@ export const folders = pgTable('folders',{
         mode:'string',
     }),
     title: text('title').notNull(),
-    inconId:text ('icon_id').notNull(),
+    iconid:text ('icon_id').notNull(),
     data: text('data'),
     inTrash:text('in_trash'),
     logo:text('logo'),
@@ -39,7 +42,7 @@ workspacesId:uuid('workspace_id').references(()=>workspaces.id, {
         mode:'string',
     }),
     title: text('title').notNull(),
-    inconId:text ('icon_id').notNull(),
+    iconid:text ('icon_id').notNull(),
     data: text('data'),
     inTrash:text('in_trash'),
     logo:text('logo'),
@@ -51,3 +54,21 @@ folderId: uuid('folder_id').references(()=>folders.id,{
     onDelete:'cascade'
 }),
  })
+
+ export const subscriptions = pgTable("subscriptions", {
+	id: text("id").primaryKey().notNull(),
+	userId: uuid("user_id").notNull().references(() => users.id),
+	status: subscriptionStatus("status"),
+	metadata: jsonb("metadata"),
+	priceId: text("price_id").references(() => prices.id),
+	quantity: integer('quantity'),
+	cancelAtPeriodEnd: boolean("cancel_at_period_end"),
+	created: timestamp("created", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
+	currentPeriodStart: timestamp("current_period_start", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
+	currentPeriodEnd: timestamp("current_period_end", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
+	endedAt: timestamp("ended_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+	cancelAt: timestamp("cancel_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+	canceledAt: timestamp("canceled_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+	trialStart: timestamp("trial_start", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+	trialEnd: timestamp("trial_end", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+});
